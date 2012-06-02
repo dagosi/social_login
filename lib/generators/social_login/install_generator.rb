62258300610a2466d3b81751b5f623fed93be25e
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-06-02 18:59:17 -0400

    Changed name generator

--- lib/generators/social_login/install_generator.rb
+++ lib/generators/social_login/install_generator.rb
@@ -2,7 +2,7 @@ require "rails/generators"
 require "fileutils"
 
 module SocialLogin
-  class OmniauthGenerator < Rails::Generators::Base
+  class InstallGenerator < Rails::Generators::Base
     source_root File.expand_path("../../templates", __FILE__)
     
     desc "Creates the omniauth file to configure the authentication."
