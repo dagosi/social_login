36062cfe839b6f80b423645ace5eb3b63a9194c6
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-06-02 13:34:34 -0400

    Added omniauth creation file.

--- lib/social_login.rb
+++ lib/social_login.rb
@@ -1,7 +1,22 @@
 require "social_login/version"
+require "social_login/translator"
 
 module SocialLogin
-  def self.hi
-    "Hello World!"
+
+  def self.create_omniauth_initializer
+    File.open("config/initializers/omniauth.rb", "w") do |file|
+      info = ""
+      info << "Rails.application.config.middleware.use OmniAuth::Builder do\n"
+      info << "  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'\n"
+      info << "  provider :facebook, 'CONSUMER_KEY', 'CONSUMER_SECRET'\n"
+      info << "  provider :linkedin, 'CONSUMER_KEY', 'CONSUMER_SECRET'\n"
+      info << "end\n"
+      file.puts info
+    end
+  end
+
+  def self.hi(language = :english)
+    translator = Translator.new(language)
+    translator.hi
   end
 end
