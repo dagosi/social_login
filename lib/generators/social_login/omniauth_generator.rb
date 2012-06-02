ae85657c5ae04b8c69587b0da8cdaac045cc5229
Daniel Gomez Sierra <danielgomezsierra@gmail.com>
2012-06-02 16:56:59 -0400

    Fixed minor bug.

--- lib/generators/social_login/omniauth_generator.rb
+++ lib/generators/social_login/omniauth_generator.rb
@@ -3,13 +3,16 @@ require "fileutils"
 
 module SocialLogin
   class OmniauthGenerator < Rails::Generators::Base
-    source_root File.expand_path("../../../", __FILE__)
-    #desc "Creates the omniauth file to configure the authentication."
+    source_root File.expand_path("../../templates", __FILE__)
+    
+    desc "Creates the omniauth file to configure the authentication."
+    
     #root = File.expand_path("../templates/", __FILE__)
     #puts root
-    def puts_hello
+    def create_omniauth_file
       puts "Hello"
+      template 'omniauth.rb', 'config/initializers/omniauth.rb'
     end
-    #Fileutils.cp("#{root}/omniauth.rb", "config/initializers/omniauth.rb")
+    #Fileutils.cp("#{root}/omniauth.rb", "")
   end
 end
